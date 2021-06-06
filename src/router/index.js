fb49152f3913a519803a1811f0ca9d9cee0e28f4
dengyanxiang <2926691109@qq.com>
2021-06-06 21:13:04 +0800

    分类参数完成

--- src/router/index.js
+++ src/router/index.js
@@ -7,13 +7,14 @@ import User from '../components/user/Users.vue'
 import Rights from '../components/power/Rights.vue'
 import Roles from '../components/power/Roles.vue'
 import Cate from '../components/goods/Cate.vue'
+import Params from '../components/goods/Params.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   { path: '/', redirect: Login },
   { path: '/login', component: Login },
-  { path: '/home', component: Home, redirect: '/welcome', children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: User }, { path: '/rights', component: Rights }, { path: '/roles', component: Roles }, { path: '/categories', component: Cate }] }
+  { path: '/home', component: Home, redirect: '/welcome', children: [{ path: '/welcome', component: Welcome }, { path: '/users', component: User }, { path: '/rights', component: Rights }, { path: '/roles', component: Roles }, { path: '/categories', component: Cate }, { path: '/params', component: Params }] }
 ]
 
 const router = new VueRouter({
